09ca9a834e93b98bb73a370ea3b21c37910444d0
Aleksandr <GreshnikAlexM@gmail.com>
2023-08-04 21:23:30 +0300

    Добавляем моды

---
+++ MyDediServer/mods/modsettings.lua
@@ -0,0 +1,21 @@
+-- Use the "ForceEnableMod" function when developing a mod. This will cause the
+-- game to load the mod every time no matter what, saving you the trouble of
+-- re-enabling it from the main menu.
+--
+-- Note! You shout NOT do this for normal mod loading. Please use the Mods menu
+-- from the main screen instead.
+
+--ForceEnableMod("kioskmode_dst")
+ForceEnableMod("351325790")
+ForceEnableMod("378160973")
+ForceEnableMod("1651623054")
+ForceEnableMod("666155465")
+
+
+-- Use "EnableModDebugPrint()" to show extra information during startup.
+
+--EnableModDebugPrint()
+
+-- Use "EnableModError()" to make the game more strict and crash on bad mod practices.
+
+--EnableModError()
